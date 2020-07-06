68cb46b7ed3bd9ba89d336b39c33151e455869f1
mifonpe <mifonpe@gmail.com>
2020-07-06 22:05:40 +0200

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -8,5 +8,5 @@ chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-git clone 
-mv
+git clone https://github.com/mifonpe/cdk-web 
+cp ./cdk-web/web/* /var/www/
