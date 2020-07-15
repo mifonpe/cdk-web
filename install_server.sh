f300036073c3676ab850a9e62f0017e60b5c0e4f
mifonpe <mifonpe@gmail.com>
2020-07-15 12:32:40 +0200

    fixing order

--- install_server.sh
+++ install_server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh 
 yum update -y
 yum install -y httpd git
+git clone https://github.com/mifonpe/cdk-web 
+cp ./cdk-web/web/* /var/www/html/
 usermod -a -G apache ec2-user
 chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-git clone https://github.com/mifonpe/cdk-web 
-cp ./cdk-web/web/* /var/www/html/
 systemctl start httpd
 systemctl enable httpd
